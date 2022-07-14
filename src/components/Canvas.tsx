16b2b69f76a41b3f8d64dc5fbe61e8c67302a2d3
Albert <albert.rong.ca@gmail.com>
2022-07-14 00:05:35 -0700

    Ported to Typescript

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,7 +4,11 @@ import { useRef, useEffect, useState } from 'react'
  * Wrapper class for strokes
  */
 class Stroke {
-  constructor(path=[]) {
+  path: number[]
+  startX: number|undefined              // these two might not want to be undefined
+  startY: number|undefined
+
+  constructor(path: number[]=[]) {
     this.path = path
 
     if (path.length !== 0) {
@@ -12,7 +16,7 @@ class Stroke {
     }
   }
 
-  addToPath(offsetX, offsetY) {
+  addToPath(offsetX: number, offsetY: number) {
     if (this.path.length === 0) {
       this.setStart(offsetX, offsetY)
     }
@@ -25,7 +29,7 @@ class Stroke {
   }
 
   // Note: Private helper, probably unnecessary.
-  setStart(startX, startY) {
+  setStart(startX: number, startY: number) {
     this.startX = startX
     this.startY = startY
   }
@@ -36,7 +40,8 @@ class Stroke {
     return {
       next: () => {
         if (index < this.getLength()) {
-          let result = {value: [this.path[index*2], this.path[index*2+1]], done: false}
+          const coordinate:[number, number] = [this.path[index*2], this.path[index*2+1]]
+          let result: {value: [number, number], done: boolean}  = {value: coordinate, done: false}  // TODO: MASSIVELY JANK
           index++
           return result
         }
@@ -47,15 +52,19 @@ class Stroke {
 }
 
 class Tile { // potentially divide up the screen to a few tiles so when erasing we only check strokes in one tile
-  strokes = []
-  constructor(x, y) { // top left
+  x: number
+  y: number
+  strokes: number[]
+
+  constructor(x: number, y: number) { // top left
     this.x = x
     this.y = y
+    this.strokes = []
   }
 }
 
 
-const Canvas = props => { // The canvas class, covers the entire window
+const Canvas = (props: {}) => { // The canvas class, covers the entire window
   
     // references to canvas and context, used for drawing
     const canvasRef = useRef(null)
@@ -66,27 +75,27 @@ const Canvas = props => { // The canvas class, covers the entire window
     let isErasing = false
 
     // saves all strokes in strokes, and saves the current stroke in currStroke
-    let strokes = []
-    const setStrokes = (s) => strokes = s
+    let strokes: Stroke[] = []
+    const setStrokes = (s: Stroke[]) => strokes = s
     let currStroke = new Stroke()
 
     // mouse events, will direct to different functions depending on button pressed
-    const mouseDown = ({nativeEvent}) => {
+    const mouseDown = ({nativeEvent}: {nativeEvent: MouseEvent}) => {  								// TODO: kinda janky, shorter solution?
       if (nativeEvent.button === 0) startDraw(nativeEvent)
       else if (nativeEvent.button === 2) startErase(nativeEvent)
     }
-    const mouseUp = ({nativeEvent}) => {
+    const mouseUp = ({nativeEvent}: {nativeEvent: MouseEvent}) => {
       if (nativeEvent.button === 0) endDraw()
       else if (nativeEvent.button === 2) endErase()
     }
-    const mouseMove = ({nativeEvent}) => {
+    const mouseMove = ({nativeEvent}: {nativeEvent: MouseEvent}) => {
       draw(nativeEvent)
       erase(nativeEvent)
     }
 
     // draws a stroke
     // when LMB is pressed, begins a new path and move it to the mouse's position
-    const startDraw = (mouseEvent) => {
+    const startDraw = (mouseEvent: MouseEvent) => {
       isDrawing = true
       const {offsetX, offsetY} = mouseEvent
       contextRef.current.beginPath()
@@ -97,7 +106,7 @@ const Canvas = props => { // The canvas class, covers the entire window
       // console.log(currStroke)
     }
     // when mouse is moving while LMB is pressed, will draw a line from last mouse position to current mouse position
-    const draw = (mouseEvent) => {
+    const draw = (mouseEvent: MouseEvent) => {
       if (!isDrawing) return
       const {offsetX, offsetY} = mouseEvent // gets current mouse position
       contextRef.current.lineTo(offsetX, offsetY)
@@ -115,8 +124,8 @@ const Canvas = props => { // The canvas class, covers the entire window
 
     // (re)draws all strokes by only drawing the difference
     // type: either 'draw' or 'erase'
-    const redraw = (strokes, type='erase') => {
-      if (strokes === undefined | strokes.length === 0) { // if no strokes then clear screen
+    const redraw = (strokes: Stroke[], type='erase') => {
+      if (strokes === undefined || strokes.length === 0) { // if no strokes then clear screen
         contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
         return 
       }
@@ -125,7 +134,7 @@ const Canvas = props => { // The canvas class, covers the entire window
       else if (type === 'erase') contextRef.current.globalCompositeOperation = 'destination-in'
 
       // adds a stroke to be redrawn
-      const addStroke = (stroke) => {
+      const addStroke = (stroke: Stroke) => {
         contextRef.current.moveTo(stroke.startX, stroke.startY)
         contextRef.current.arc(stroke.startX, stroke.startY, .5, 0, Math.PI*2) // draws a circle at the starting position
         for (let i = 1; i < stroke.getLength(); i++) {
@@ -145,15 +154,15 @@ const Canvas = props => { // The canvas class, covers the entire window
      // keeps track of the last mouse position so erase won't trigger if mouse did not move much
     let lastX = 0, lastY = 0
 
-    const startErase = (mouseEvent) => {
+    const startErase = (mouseEvent: MouseEvent) => {
       console.log("# of strokes: ", strokes.length)
       isErasing = true
       erase(mouseEvent)
     }
     // loops through all arrays in strokes and remove any stroke close to the mouse
     // when mouse is moving and RMB is pressed
-    const erase = (mouseEvent) => {
-      if (!isErasing | strokes.length === 0) return
+    const erase = (mouseEvent: MouseEvent) => {
+      if (!isErasing || strokes.length === 0) return
       const {offsetX, offsetY} = mouseEvent // gets current mouse position
       if (withinSquare(offsetX, offsetY, lastX, lastY, 5)) return // if mouse didn't move much then we won't recheck
       lastX = offsetX
@@ -165,9 +174,11 @@ const Canvas = props => { // The canvas class, covers the entire window
       loop1:
       for (let i = strokes.length-1; i >=0 ; i--) { // loops through each stroke in strokes
         for (const coord of strokes[i]) {
+          if (typeof(coord) === 'number') {  														// TODO: EXTREME JANKNESS
+			break;
+		  }
           if (withinSquare(offsetX, offsetY, coord[0], coord[1], size)) {
             allStrokes.splice(i, 1) // if a stroke is within size, remove it from allStrokes
-
             // redraws all strokes left in allStrokes
             redraw(allStrokes, 'erase')
             setStrokes(allStrokes) // update strokes, removing the ones deleted
@@ -200,8 +211,8 @@ const Canvas = props => { // The canvas class, covers the entire window
     }, [])
 
     // returns if 2 coords are within a 'length' of each other
-    const withinSquare = (x1, y1, x2, y2, length) => {
-      return Math.abs(x1-x2) <= length & Math.abs(y1-y2) <= length
+    const withinSquare = (x1: number, y1: number, x2: number, y2: number, length: number) => {
+      return Math.abs(x1-x2) <= length && Math.abs(y1-y2) <= length
     }
   
   return (
